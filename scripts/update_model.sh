b6b3c2a3872f6637e23d849c983943d6ac3eb6f2
gezp <zhenpeng.ge@qq.com>
2021-10-30 09:47:41 +0800

    update model

--- scripts/update_model.sh
+++ scripts/update_model.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 path=`pwd`
 for dir in $path/../resource/models/*; do
+    echo "parse "$dir
     cd $dir
-    echo $dir
-    xmacro4sdf model.sdf.xmacro
+    tempfile=`mktemp temp.XXXXXX`
+    xmacro4sdf model.sdf.xmacro > $tempfile
+    lines_num=`cat $tempfile | wc -l`
+    if (($lines_num > 3 )) ;then
+        cat $tempfile > model.sdf
+    else
+        cat $tempfile
+    fi
+    rm -f temp.*
 done
