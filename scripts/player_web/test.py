53adffd66169e4652c6e0b7942ce6edafa3cad77
WUyinwei-hah <WYW3276027@126.com>
2021-11-07 20:36:26 +0800

    add web controller (#3)
    * add web controller
    
    add web controller
    
    * delete redundant  files
    
    delete redundant  files
    
    Co-authored-by: Obsession <6535155+W3276027@user.noreply.gitee.com>

---
+++ scripts/player_web/test.py
@@ -0,0 +1,62 @@
+# ==========================================
+#   packages for ros
+# ==========================================
+import rclpy
+import sys
+from sensor_msgs.msg import Image
+import cv2
+import base64
+import time
+import numpy as np
+from rmoss_interfaces.msg import ChassisCmd
+from rmoss_interfaces.msg import GimbalCmd
+from rmoss_interfaces.msg import ShootCmd
+
+# ==========================================
+#    ros functions
+# ==========================================
+from ros_handler import *
+
+# ==========================================
+#    robot state
+# ==========================================
+gimbal_pitch = 0.0
+gimbal_yaw = 0.0
+speed = 1.0
+
+# ==========================================
+#    pubs
+# ==========================================
+node = None
+robot_name = None
+# chassis_cmd_pub = 0
+# gimbal_cmd_pub = 0
+# shoot_cmd_pub = 0
+
+def send_instruction(chassis_x, chassis_y, gimbal_pitch, gimbal_yaw):
+    # publish_chassis_cmd_msg(chassis_cmd_pub, chassis_x, chassis_y, 0.0)
+    publish_gimbal_cmd_msg(gimbal_cmd_pub, 0.0, gimbal_yaw)
+
+
+if __name__ == '__main__':
+    # global node, chassis_cmd_pub, gimbal_cmd_pub, shoot_cmd_pub
+
+    #ROS2 Task
+    rclpy.init()
+    node=rclpy.create_node('player_web')
+    #standard_robot_red1,standard_robot_blue1
+    robot_name="standard_robot_red1"
+    port=5000
+    if len(sys.argv) == 3:
+        robot_name = str(sys.argv[1])
+    print("Robot name:",robot_name, "Port:",port)
+    chassis_cmd_pub = node.create_publisher(ChassisCmd, '/%s/robot_base/chassis_cmd' % (robot_name), 10)
+    gimbal_cmd_pub = node.create_publisher(GimbalCmd, '/%s/robot_base/gimbal_cmd' % (robot_name), 10)
+    shoot_cmd_pub = node.create_publisher(ShootCmd, '/%s/robot_base/shoot_cmd' % (robot_name), 10)
+    gimbal_pitch = 0.0
+    gimbal_yaw = 0.0
+    while True:
+        send_instruction(0.0, 0.0, 0.0, gimbal_yaw)
+        gimbal_pitch = gimbal_pitch + 0.1
+        gimbal_yaw = gimbal_yaw + 0.1
+        time.sleep(1)
