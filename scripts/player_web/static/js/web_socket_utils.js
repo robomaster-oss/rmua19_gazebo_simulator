e412b4bde46f7c72e6207fc75ff9cf5ea56c3f6f
Obsession <6535155+W3276027@user.noreply.gitee.com>
2021-11-09 22:01:56 +0800

    删除了无用代码
    删除了无用代码

--- scripts/player_web/static/js/web_socket_utils.js
+++ scripts/player_web/static/js/web_socket_utils.js
@@ -2,43 +2,12 @@
 // 初始化连接
 //------------------------------------
 function init_socket() {
-    // var start_time;
-    // var i = 0
-    // var ping_arr = []
-    // var my_camera = document.getElementById("camera")
-    // var socket = io();
     window.socket = io();
-
     socket.on('connect', function () {
         console.log('connect')
         $('#start_btn').removeAttr("disabled");
         $('#start_btn').text("开始游戏");
-        // socket.on('image', function (message) {
-        //     i++
-        //     my_camera.setAttribute('src', 'data:image/png;base64,' + message.img)
-        // });
-        // window.setInterval(() => {
-        //     start_time = (new Date).getTime();
-        //     console.log("帧率:" + i)
-        //     $('#fps').text(i);
-        //     i = 0
-        //     socket.emit('ping');
-        // }, 1000)
-        // socket.on('pong', function () {
-        //     console.log('pong')
-        //     var latency = (new Date).getTime() - start_time;
-        //     ping_arr.push(latency);
-        //     ping_arr = ping_arr.slice(-30);
-        //     var sum = 0;
-        //     for (var i = 0; i < ping_arr.length; i++)
-        //         sum += ping_arr[i];
-        //     ping = Math.round(10 * sum / ping_arr.length) / 10
-        //     console.log(ping)
-        //     $('#ping').text(ping + 'ms');
-        // });
-        // timer = setInterval(() => {
-        //     socket.emit('control', active_map);
-        // }, send_freq)
+
     });
 
     socket.on('disconnect', function () {
