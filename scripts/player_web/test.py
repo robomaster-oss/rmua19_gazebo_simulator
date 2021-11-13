e7164be9f75538279946a52f499158e65f8bb71a
Obsession <6535155+W3276027@user.noreply.gitee.com>
2021-11-13 15:31:20 +0800

    add_lifebar
    增加了以下功能
    1：显示血条
    2：显示指针
    3：第一人称控制

--- scripts/player_web/test.py
+++ scripts/player_web/test.py
@@ -11,7 +11,7 @@ import numpy as np
 from rmoss_interfaces.msg import ChassisCmd
 from rmoss_interfaces.msg import GimbalCmd
 from rmoss_interfaces.msg import ShootCmd
-
+from std_msgs.msg import Int32, String
 # ==========================================
 #    ros functions
 # ==========================================
@@ -33,10 +33,12 @@ robot_name = None
 # gimbal_cmd_pub = 0
 # shoot_cmd_pub = 0
 
-def send_instruction(chassis_x, chassis_y, gimbal_pitch, gimbal_yaw):
+def  send_instruction(chassis_x, chassis_y, gimbal_pitch, gimbal_yaw):
     # publish_chassis_cmd_msg(chassis_cmd_pub, chassis_x, chassis_y, 0.0)
     publish_gimbal_cmd_msg(gimbal_cmd_pub, 0.0, gimbal_yaw)
 
+def log_func(msg):
+    print(msg.data)
 
 if __name__ == '__main__':
     # global node, chassis_cmd_pub, gimbal_cmd_pub, shoot_cmd_pub
@@ -49,14 +51,23 @@ if __name__ == '__main__':
     port=5000
     if len(sys.argv) == 3:
         robot_name = str(sys.argv[1])
-    print("Robot name:",robot_name, "Port:",port)
-    chassis_cmd_pub = node.create_publisher(ChassisCmd, '/%s/robot_base/chassis_cmd' % (robot_name), 10)
-    gimbal_cmd_pub = node.create_publisher(GimbalCmd, '/%s/robot_base/gimbal_cmd' % (robot_name), 10)
-    shoot_cmd_pub = node.create_publisher(ShootCmd, '/%s/robot_base/shoot_cmd' % (robot_name), 10)
-    gimbal_pitch = 0.0
-    gimbal_yaw = 0.0
-    while True:
-        send_instruction(0.0, 0.0, 0.0, gimbal_yaw)
-        gimbal_pitch = gimbal_pitch + 0.1
-        gimbal_yaw = gimbal_yaw + 0.1
-        time.sleep(1)
+    # print("Robot name:",robot_name, "Port:",port)
+    # chassis_cmd_pub = node.create_publisher(ChassisCmd, '/%s/robot_base/chassis_cmd' % (robot_name), 10)
+    # gimbal_cmd_pub = node.create_publisher(GimbalCmd, '/%s/robot_base/gimbal_cmd' % (robot_name), 10)
+    # shoot_cmd_pub = node.create_publisher(ShootCmd, '/%s/robot_base/shoot_cmd' % (robot_name), 10)
+    blue_hp_sub = node.create_subscription(Int32, '/referee_system/standard_robot_blue1/hp', log_func,
+                                       10)
+    red_hp_sub = node.create_subscription(Int32, '/referee_system/standard_robot_red1/hp', log_func,
+                                       10)
+    attack_info_sub = node.create_subscription(String, '/referee_system/attack_info', log_func,
+                                       10)
+    rclpy.spin(node)
+    node.destroy_node()
+    # gimbal_pitch = 0.0
+    # gimbal_yaw = 0.0
+    # while True:
+    #     # send_instruction(0.0, 0.0, 0.0, gimbal_yaw)
+    #     # # gimbal_pitch = gimbal_pitch + 0.1
+    #     # gimbal_yaw = gimbal_yaw + 0.2
+    #     # print(gimbal_yaw)
+    #     time.sleep(0.5)
